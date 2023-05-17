ce6dacf92686e249e0ee1714cdcca2c0e0c05e08
Alex Piet <alexpiet@gmail.com>
2023-05-17 16:59:36 -0700

    pip organization

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='licking-behavior-np',
+      packages=['licking_behavior_NP'],
+      version='1.0.0',
+      description='GLM for Visual Behavior ophys data',
+      url='https://github.com/AllenInstitute/licking_behavior_NP',
+      author='Alex Piet',
+      author_email="alex.piet@alleninstitute.org",
+      license='Allen Institute',
+     )
