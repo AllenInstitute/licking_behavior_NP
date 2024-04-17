e015c9095489588c191d3109bef2650161319f55
Roberto <37729096+RobertoDF@users.noreply.github.com>
2024-04-17 13:28:11 +0200

    Isolate active task in stimulus_presentation df

--- licking_behavior_NP/psy_general_tools.py
+++ licking_behavior_NP/psy_general_tools.py
@@ -82,7 +82,7 @@ def get_data(bsid):
 
     # Remove Passive session:
     print('removing passive session stimuli')
-    session.stimulus_presentations_np = session.stimulus_presentations.query('active')
+    session.stimulus_presentations_np = session.stimulus_presentations[(session.stimulus_presentations["stimulus_block"]==0)]
     session.stimulus_presentations_np['omitted'] \
         = session.stimulus_presentations_np['omitted'].astype(bool)
 
